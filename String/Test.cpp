467b5e02533a799a03486b966394363493e64b72
MANDYZML <manlinzhang4@gmail.com>
2022-08-12 21:47:12 +0900

    8/12 更新

--- String/Test.cpp
+++ String/Test.cpp
@@ -304,51 +304,51 @@ void test_string3()
 }
 
 //迭代器-- iterator(像指针一样的类型 以及用法)  遍历
-void test_string4()
-{
-	string s("hello");
-	string::iterator it = s.begin();//返回第一个位置的迭代器
-	while (it != s.end())//end 返回最后一个位置的下一个 也就是\0
-	{
-		cout << *it << " ";
-		++it;
-	}
-	cout << endl;
-
-	//范围for -- 自动迭代(++) 自动判断结束
-	//依次取s中的每个字符 赋值给ch
-	/*for (auto ch : s)
-	{
-		ch++;
-		cout << ch << " ";
-	}*/
-	for (auto& ch : s)
-	{
-		ch++;
-		cout << ch << " ";
-	}
-	cout << endl;
-
-	cout << s << endl;
-
-	//列表
-	/*List<int> lt(10, 1);
-	List<int> :: iterator lit = lt.begin();
-	while (lit != lt.end())
-	{
-		cout << *lit << " ";
-		++lit;
-	}
-	cout << endl;*/
-
-	for (auto e : lt)
-	{
-		cout << e << " ";
-	}
-	cout << endl;
-
-	//范围for底层其实就是迭代器
-}
+//void test_string4()
+//{
+//	string s("hello");
+//	string::iterator it = s.begin();//返回第一个位置的迭代器
+//	while (it != s.end())//end 返回最后一个位置的下一个 也就是\0
+//	{
+//		cout << *it << " ";
+//		++it;
+//	}
+//	cout << endl;
+//
+//	//范围for -- 自动迭代(++) 自动判断结束
+//	//依次取s中的每个字符 赋值给ch
+//	/*for (auto ch : s)
+//	{
+//		ch++;
+//		cout << ch << " ";
+//	}*/
+//	for (auto& ch : s)
+//	{
+//		ch++;
+//		cout << ch << " ";
+//	}
+//	cout << endl;
+//
+//	cout << s << endl;
+//
+//	//列表
+//	/*List<int> lt(10, 1);
+//	List<int> :: iterator lit = lt.begin();
+//	while (lit != lt.end())
+//	{
+//		cout << *lit << " ";
+//		++lit;
+//	}
+//	cout << endl;*/
+//
+//	for (auto e : lt)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//
+//	//范围for底层其实就是迭代器
+//}
 
 void PrintString(string& str)
 {
@@ -405,16 +405,182 @@ void test_string6()
 	cout << copy << endl;
 }
 
-void test_string7()
+//void test_string7()
+//{
+//	string s1;
+//	string s2("1111111111111");
+//	cout << s1.max_size() << endl;
+//	cout << s2.max_size() << endl;
+//
+//	//容量大小
+//	cout << s1.capacity() << endl;
+//	cout << s2.capacity() << endl; 
+//
+//	//reverse 逆置
+//	//s.reserve(1000); //保留 开空间
+//	s.resize(1000)  //开空间+初始化
+//}
+
+void test_string8()
 {
-	string s1;
-	string s2("1111111111111");
-	cout << s1.max_size() << endl;
-	cout << s2.max_size() << endl;
+	//每个空格的位置插入一个 %
+	string str("wo lai le");
+	//for (size_t i = 0; i < str.size();)
+	//{
+	//	if (str[i] == ' ')
+	//	{
+	//		str.insert(i, "%20");//给空格位置插入%20
+	//		i += 4;
+	//	}
+	//	else
+	//	{
+	//		//不等于空格
+	//		++i;
+	//	}
+	//	
+	//}
+
+	//for (size_t i = 0; i < str.size();++i)
+	//{
+	//	if (str[i] == ' ')
+	//	{
+	//		str.insert(i, "%20");//给空格位置插入%20
+	//		i += 3;
+	//	}
+
+	//}
+	/*cout << str << endl;*/
 
 
-	cout << s1.capacity() << endl;
-	cout << s2.capacity() << endl; 
+	//删除字符 字符如果短 删除范围多 就有多少删多少
+//	for (size_t i = 0; i < str.size(); ++i)
+//	{
+//		if (str[i] == ' ')
+//		{
+//			str.erase(i, 1);//从空格开始删 删一个
+//		}
+//	}
+//	cout << str << endl;
+
+	//以空间换时间  O(N)
+	string newstr;
+	for (size_t i = 0; i < str.size(); ++i)//遍历字符串
+	{
+		if (str[i] != ' ')
+		{
+			newstr += str[i];
+		}
+		else
+		{
+			//等于空格就插入
+			newstr += "20%";
+		}
+	}
+	cout << newstr << endl;
+}
+
+
+void test_string9()
+{
+	//用c读文件
+	string  filename("test.cpp");
+	cout << filename << endl;
+	cout << filename.c_str() << endl;
+
+
+	FILE* fout = fopen(filename.c_str(), "r");
+	assert(fout);
+	char ch = fgetc(fout);
+	while (ch != EOF)
+	{
+		cout << ch;
+		ch = fgetc(fout);//打印所有test.cpp中的代码
+	}
+}
+
+void test_string10()
+{
+	//用c读文件
+	string  filename("test.cpp");
+	cout << filename << endl;
+	cout << filename.c_str() << endl;
+
+	filename += '\0';
+	filename += "string.cpp";
+	cout << filename << endl; //以string 对象 size 为准
+	cout << filename.c_str() << endl;//以常量字符串对象 \0为准 遇到\0就终止
+
+	cout << filename.size() << endl;
+	string copy = filename;
+	cout << copy << endl << endl;
+	
+	for (unsigned ch = 0; ch < 128; ++ch)
+	{
+		cout << ch;
+	}
+	cout << endl;
+	cout << "\0" << endl;//打印不出来 两个字符 \ 0
+}
+
+void DealUrl(const string& url)
+{
+	//第一个部分 取协议长度
+	size_t pos1 = url.find("://");
+	if (pos1 == string::npos)
+	{
+		cout << "非法url" << endl;
+		return;
+	}
+	string protocol = url.substr(0, pos1);//取协议长度
+	cout << protocol << endl;
+
+	//第二部分 -- 取域名
+	size_t pos2 = url.find('/', pos1 + 3);//找下一个/
+	if (pos2 == string::npos)
+	{
+		cout << "非法url" << endl;
+		return;
+	}
+
+	//从pos1+3 开始取 取到下一个/位置
+	string domain = url.substr(pos1 + 3, pos2 - pos1 - 3);
+	cout << domain << endl;
+
+	//第三部分 -- 资源定位
+	string uri = url.substr(pos2 + 1);//取到结尾
+	cout << uri << endl;
+}
+void test_string11()
+{
+	//找文件后缀
+	string filename("test.cpp.tar.zip");
+	//size_t pos = filename.find('.');//根据. 来找
+	//if (pos != string::npos)
+	//{
+	//	//从pos位置开始取 length个长度
+	//	//.size 就是后缀长度 .cpp
+	//	//string suff = filename.substr(pos, filename.size() - pos);
+	//	string suff = filename.substr(pos);//找到pos点 直接取到末尾
+	//	cout << suff << endl;
+	//}
+
+	//只取最后一个后缀
+	size_t pos = filename.rfind('.');//倒着找 取最后一个后缀
+	if (pos != string::npos)
+	{
+		//从pos位置开始取 length个长度
+		//.size 就是后缀长度 .cpp
+		//string suff = filename.substr(pos, filename.size() - pos);
+		string suff = filename.substr(pos);//找到pos点 直接取到末尾
+		cout << suff << endl;
+	}
+
+	string url1 = "https://leetcode.cn/problems/reverse-only-letters/submissions/";
+	string url2 = "file:///C:/Users/manli/OneDrive/%E6%A1%8C%E9%9D%A2/IT%E5%90%84%E7%A7%8D%E8%AF%BE%E4%BB%B6/%E6%AF%94%E7%89%B9/C++%E8%AF%BE%E4%BB%B6/C++%E8%AF%BE%E4%BB%B6--2022%E4%BF%AE%E8%AE%A2/C++%E8%AF%BE%E4%BB%B6--2022%E4%BF%AE%E8%AE%A2/C++%E5%88%9D%E9%98%B6%E8%AF%BE%E4%BB%B6/Lesson08---string.pdf";
+
+	//把网址分割成三个部分
+	DealUrl(url1);
+	
 }
 
 int main()
@@ -425,6 +591,10 @@ int main()
 	//test_string4();
 	//test_string5();
 	//test_string6();
-	test_string7();
+	//test_string7();
+	//test_string8();
+	//test_string9();
+	//test_string10();
+	test_string11();
 	return 0;
 }
